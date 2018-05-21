ba0d5378dfff00780a3b89f637bab9d65a3bf722
douglasjd1 <douglasjd09@gmail.com>
2018-05-21 16:55:40 -0500

    security issues

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -1,12 +1,28 @@
 package controllers;
 
+import play.data.DynamicForm;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.inject.Inject;
+
 public class GameController extends Controller
 {
+    private FormFactory formFactory;
+
+    @Inject
+    public GameController(FormFactory formFactory)
+    {
+        this.formFactory = formFactory;
+    }
     public Result getWelcome()
     {
+        DynamicForm form = formFactory.form().bindFromRequest();
+        String name = form.get("name");
+
+        session().put("name", name);
         return ok(views.html.welcome.render());
     }
 
@@ -17,32 +33,32 @@ public class GameController extends Controller
 
     public Result postEastFromEngland()
     {
-        return ok(views.html.eastfromengland.render());
+        return ok(views.html.eastfromengland.render(session().get("name")));
     }
 
     public Result postNorthFromEngland()
     {
-        return ok(views.html.northfromengland.render());
+        return ok(views.html.northfromengland.render(session().get("name")));
     }
 
     public Result postWestFromEngland()
     {
-        return ok(views.html.westfromengland.render());
+        return ok(views.html.westfromengland.render(session().get("name")));
     }
 
     public Result postEastEnd()
     {
-        return ok(views.html.eastend.render());
+        return ok(views.html.eastend.render(session().get("name")));
     }
 
     public Result postWestEnd()
     {
-        return ok(views.html.westend.render());
+        return ok(views.html.westend.render(session().get("name")));
     }
 
     public Result postHomePort()
     {
-        return ok(views.html.homeport.render());
+        return ok(views.html.homeport.render(session().get("name")));
     }
 
     public Result getKittens()
