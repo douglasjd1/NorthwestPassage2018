7fb333f023cc1d989c8d6e92a3fcbb95a28e4ffe
douglasjd1 <douglasjd09@gmail.com>
2018-05-22 09:34:07 -0500

    Northwest passage some silver, some gold

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -5,13 +5,16 @@ import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
+import scala.Int;
+import views.html.helper.form;
 
 import javax.inject.Inject;
+import java.util.Random;
 
 public class GameController extends Controller
 {
     private FormFactory formFactory;
-
+    private final String PLAYER_NAME_KEY = "name";
     @Inject
     public GameController(FormFactory formFactory)
     {
@@ -19,46 +22,65 @@ public class GameController extends Controller
     }
     public Result getWelcome()
     {
-        DynamicForm form = formFactory.form().bindFromRequest();
-        String name = form.get("name");
-
-        session().put("name", name);
         return ok(views.html.welcome.render());
     }
 
     public Result postStart()
     {
-        return ok(views.html.start.render());
+        DynamicForm form = formFactory.form().bindFromRequest();
+        String name = form.get("name");
+        session().put(PLAYER_NAME_KEY, name);
+        session().put("crewMembers", "10");
+        return ok(views.html.start.render(session().get("name")));
+    }
+
+    public void lostCrewMember()
+    {
+        int crewMembers = Integer.parseInt(session().get("crewMembers")) - 1;
+        session().put("crewMembers", String.valueOf(crewMembers));
     }
 
     public Result postEastFromEngland()
     {
-        return ok(views.html.eastfromengland.render(session().get("name")));
+        lostCrewMember();
+        if(Integer.parseInt(session().get("crewMembers")) < 5)
+        {
+            return ok(views.html.oakisland.render(session().get("name"), Integer.parseInt(session().get("crewMembers"))));
+        }
+        else
+        {
+            return ok(views.html.eastfromengland.render(session().get("name"), Integer.parseInt(session().get("crewMembers"))));
+        }
     }
 
     public Result postNorthFromEngland()
     {
-        return ok(views.html.northfromengland.render(session().get("name")));
+        return ok(views.html.northfromengland.render(session().get("name"), Integer.parseInt(session().get("crewMembers"))));
     }
 
     public Result postWestFromEngland()
     {
-        return ok(views.html.westfromengland.render(session().get("name")));
+        lostCrewMember();
+        return ok(views.html.westfromengland.render(session().get("name"), Integer.parseInt(session().get("crewMembers"))));
     }
 
     public Result postEastEnd()
     {
-        return ok(views.html.eastend.render(session().get("name")));
+        return ok(views.html.eastend.render(session().get("name"), Integer.parseInt(session().get("crewMembers"))));
     }
 
     public Result postWestEnd()
     {
-        return ok(views.html.westend.render(session().get("name")));
+        Random random = new Random();
+        int chance = random.nextInt(4) + 1;
+        if(chance == 1)
+            return ok(views.html.meetingwithsanta.render(session().get("name"), Integer.parseInt(session().get("crewMembers"))));
+        return ok(views.html.westend.render(session().get("name"), Integer.parseInt(session().get("crewMembers"))));
     }
 
     public Result postHomePort()
     {
-        return ok(views.html.homeport.render(session().get("name")));
+        return ok(views.html.homeport.render(session().get("name"), Integer.parseInt(session().get("crewMembers"))));
     }
 
     public Result getKittens()
