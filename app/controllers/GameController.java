2ea5ed50a2d90e22bd73b9f4f54a0b0e77d885c9
douglasjd1 <douglasjd09@gmail.com>
2018-05-22 12:51:04 -0500

    Northwest passage, gold finished

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -32,19 +32,35 @@ public class GameController extends Controller
     public Result postStart()
     {
         crewMembers.clear();
-        initializeCrewMembers();
+
+        if(session().get("name").equals("Darth Vader"))
+            initializeVaderCrew();
+        else
+            initializeCrewMembers();
+
         DynamicForm form = formFactory.form().bindFromRequest();
         String name = form.get("name");
         session().put("name", name);
+        String kittenImage = form.get("kitten");
+        session().put("kitten", kittenImage + ".jpg");
 
-        return ok(views.html.start.render(session().get("name"),crewMembers.size(), crewMembers));
+        if(name.equals("Darth Vader"))
+        {
+            session().put("kitten", "eastereggvader.jpg");
+            return ok(views.html.eastereggvader.render(session("kitten")));
+        }
+
+        return ok(views.html.start.render(session().get("name"),crewMembers.size(), crewMembers, session("kitten")));
     }
 
     public Result getStart()
     {
         crewMembers.clear();
-        initializeCrewMembers();
-        return ok(views.html.start.render(session().get("name"),crewMembers.size(), crewMembers));
+        if(session().get("name").equals("Darth Vader"))
+            initializeVaderCrew();
+        else
+            initializeCrewMembers();
+        return ok(views.html.start.render(session().get("name"),crewMembers.size(), crewMembers, session("kitten")));
     }
 
     public String lostCrewMember()
@@ -73,11 +89,11 @@ public class GameController extends Controller
         String injuredCrewMember = lostCrewMember();
         if(crewMembers.size() < 5)
         {
-            return ok(views.html.oakisland.render(session().get("name"),crewMembers.size(), crewMembers));
+            return ok(views.html.oakisland.render(session().get("name"),crewMembers.size(), crewMembers, session("kitten")));
         }
         else
         {
-            return ok(views.html.eastfromengland.render(session().get("name"), crewMembers.size(), injuredCrewMember, crewMembers));
+            return ok(views.html.eastfromengland.render(session().get("name"), crewMembers.size(), injuredCrewMember, crewMembers, session("kitten")));
         }
     }
 
@@ -86,19 +102,19 @@ public class GameController extends Controller
         Random random = new Random();
         int santaChance = random.nextInt(4) + 1;
         if(santaChance == 1)
-            return ok(views.html.meetingwithsanta.render(session().get("name"),crewMembers.size(), crewMembers));
-        return ok(views.html.northfromengland.render(session().get("name"),crewMembers.size(), crewMembers));
+            return ok(views.html.meetingwithsanta.render(session().get("name"),crewMembers.size(), crewMembers, session("kitten")));
+        return ok(views.html.northfromengland.render(session().get("name"),crewMembers.size(), crewMembers, session("kitten")));
     }
 
     public Result postWestFromEngland()
     {
         String injuredCrewMember = lostCrewMember();
-        return ok(views.html.westfromengland.render(session().get("name"), crewMembers.size(), injuredCrewMember, crewMembers));
+        return ok(views.html.westfromengland.render(session().get("name"), crewMembers.size(), injuredCrewMember, crewMembers, session("kitten")));
     }
 
     public Result postEastEnd()
     {
-        return ok(views.html.eastend.render(session().get("name"),crewMembers.size(), crewMembers));
+        return ok(views.html.eastend.render(session().get("name"),crewMembers.size(), crewMembers, session("kitten")));
     }
 
     public Result postWestEnd()
@@ -107,17 +123,36 @@ public class GameController extends Controller
         int leadChance = random.nextInt(5) + 1;
 
         if(leadChance == 1)
-            return ok(views.html.westendleadpaint.render(session().get("name"),crewMembers.size(), crewMembers));
-        return ok(views.html.westend.render(session().get("name"),crewMembers.size(), crewMembers));
+            return ok(views.html.westendleadpaint.render(session().get("name"),crewMembers.size(), crewMembers, session("kitten")));
+        return ok(views.html.westend.render(session().get("name"),crewMembers.size(), crewMembers, session("kitten")));
     }
 
     public Result postHomePort()
     {
-        return ok(views.html.homeport.render(session().get("name"),crewMembers.size(), crewMembers));
+        return ok(views.html.homeport.render(session().get("name"),crewMembers.size(), crewMembers, session("kitten")));
     }
 
     public Result getKittens()
     {
         return ok(views.html.kittens.render());
+    }
+
+    public Result postEasterEggVader()
+    {
+        return ok(views.html.eastereggvader.render(session("kitten")));
+    }
+
+    private void initializeVaderCrew()
+    {
+        crewMembers.add("Darth Sideous");
+        crewMembers.add("Padme Amidala");
+        crewMembers.add("Jar Jar Binks");
+        crewMembers.add("Royal Guard");
+        crewMembers.add("Boba Fett");
+        crewMembers.add("Lando Clarissian");
+        crewMembers.add("Grand Moff Tarkin");
+        crewMembers.add("Darth Maul");
+        crewMembers.add("Asajj Ventress");
+        crewMembers.add("Savage Oppress");
     }
 }
